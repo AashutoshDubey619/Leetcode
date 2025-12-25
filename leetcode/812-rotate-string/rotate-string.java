6484fa9d40108cc122d1eca506fc6226002e6f50
AashutoshDubey619 <aashutoshd85@gmail.com>
2025-12-25 14:51:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (78.00%) - LeetSync

--- leetcode/812-rotate-string/rotate-string.java
+++ leetcode/812-rotate-string/rotate-string.java
@@ -1,21 +1,7 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-
-        if (s.length() != goal.length()) return false;
-
-        char[] arr = s.toCharArray();
-
-        for (int i = 0; i < arr.length; i++) {
-            char first = arr[0];
-            for (int j = 0; j < arr.length - 1; j++) {
-                arr[j] = arr[j + 1];
-            }
-            arr[arr.length - 1] = first;
-
-            if (new String(arr).equals(goal))
-                return true;
-        }
-
-        return false;
+        if(s.length()!=goal.length()) return false;
+        String d = s+s;
+        return d.contains(goal); 
     }
 }
