184a9c1a49f4ff8871ab73a9b045114ac61725f5
AashutoshDubey619 <aashutoshd85@gmail.com>
2025-12-27 23:00:40 +0530

    Time: 16 ms (35.05%) | Memory: 47.6 MB (49.69%) - LeetSync

---
+++ leetcode/347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int x : nums) freq.put(x, freq.getOrDefault(x, 0) + 1);
+
+        // min-heap by frequency
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(freq.get(a), freq.get(b))
+        );
+
+        for (int key : freq.keySet()) {
+            pq.offer(key);
+            if (pq.size() > k) pq.poll();
+        }
+
+        // poll to get exactly k elements (any order is fine for LC-347)
+        int[] res = new int[pq.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+}
