08d423c4868c236916ac748163e224819fcf1670
AashutoshDubey619 <aashutoshd85@gmail.com>
2025-12-29 19:36:54 +0530

    Time: 3 ms (98.73%) | Memory: 47.5 MB (15.74%) - LeetSync

--- leetcode/1028-interval-list-intersections/interval-list-intersections.java
+++ leetcode/1028-interval-list-intersections/interval-list-intersections.java
@@ -7,19 +7,25 @@ class Solution {
 
         List<int[]> res = new ArrayList<>();
 
-        for (int i = 0; i < firstList.length; i++) {
+        int i = 0, j = 0;
+
+        while (i < firstList.length && j < secondList.length) {
+
             int[] prev = firstList[i];
+            int[] curr = secondList[j];
 
-            for (int j = 0; j < secondList.length; j++) {
-                int[] curr = secondList[j];
+            
+            if (curr[0] <= prev[1] && curr[1] >= prev[0]) {
+                int start = Math.max(prev[0], curr[0]);
+                int end   = Math.min(prev[1], curr[1]);
+                res.add(new int[]{start, end});
+            }
 
-                if (curr[0] <= prev[1] && curr[1] >= prev[0]) {
-
-                    int start = Math.max(prev[0], curr[0]);
-                    int end   = Math.min(prev[1], curr[1]);
-
-                    res.add(new int[]{start, end});
-                }
+           
+            if (prev[1] < curr[1]) {
+                i++;
+            } else {
+                j++;
             }
         }
 
