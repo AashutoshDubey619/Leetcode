997cdd6432358d43813619abb60bcc874dae50b2
AashutoshDubey619 <aashutoshd85@gmail.com>
2025-12-31 11:20:45 +0530

    Time: 6 ms (91.65%) | Memory: 46.8 MB (5.01%) - LeetSync

--- leetcode/1222-remove-covered-intervals/remove-covered-intervals.java
+++ leetcode/1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,28 +1,22 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        
+
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) return b[1] - a[1]; 
+            return a[0] - b[0];
+        });
+
         int count = 0;
+        int maxEnd = intervals[0][1];
 
-        for(int i=0;i<intervals.length;i++){
-            boolean isCovered = false;
-
-            for(int j=0;j<intervals.length;j++){
-                if(j == i) continue;
-
-                int[] prev = intervals[i];
-                int[] curr = intervals[j];
-
-                if(curr[0] <= prev[0] && curr[1] >= prev[1]){
-                    isCovered = true;
-                }
-            }
-
-            if(!isCovered){
-                count++;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][1] <= maxEnd) {
+                count++; 
+            } else {
+                maxEnd = intervals[i][1];
             }
         }
 
-        return count;
-
+        return intervals.length - count; 
     }
 }
