9c6bbe1c3a03a27e69c420afde98d1a1588bf4f5
AashutoshDubey619 <aashutoshd85@gmail.com>
2026-01-02 20:39:29 +0530

    Time: 5 ms (83.76%) | Memory: 47.2 MB (11.64%) - LeetSync

---
+++ leetcode/155-min-stack/min-stack.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    private Stack<Integer> stk;
+    private Stack<Integer> minstk;
+
+    public MinStack() {
+        stk = new Stack<>();
+        minstk = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stk.push(val);
+
+        if (minstk.isEmpty() || val <= minstk.peek()) {
+            minstk.push(val);
+        }
+    }
+    
+    public void pop() {
+        int removed = stk.pop();
+
+        if (removed == minstk.peek()) {
+            minstk.pop();
+        }
+    }
+    
+    public int top() {
+        return stk.peek();
+    }
+    
+    public int getMin() {
+        return minstk.peek();
+    }
+}
