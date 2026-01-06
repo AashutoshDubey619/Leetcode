e05c4e4fadcb8638300d6405fa6b726e1c3c6819
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-01-06 20:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (38.06%) - LeetSync

--- leetcode/232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ leetcode/232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,31 +1,56 @@
 class MyQueue {
+    private Stack<Integer>stk1;
+    private Stack<Integer>stk2;
 
-    private Stack<Integer> s1 = new Stack<>();
-    private Stack<Integer> s2 = new Stack<>();
-
+    public MyQueue() {
+        stk1 = new Stack<>();
+        stk2 = new Stack<>();
+    }
+    
     public void push(int x) {
-        s1.push(x);
+        stk1.push(x);
     }
-
+    
     public int pop() {
-        if (s2.isEmpty()) {
-            while (!s1.isEmpty()) {
-                s2.push(s1.pop());
+        if(!stk1.isEmpty()){
+            while(!stk1.isEmpty()){
+                stk2.push(stk1.pop());
             }
         }
-        return s2.pop();
-    }
+        int x = stk2.pop();
 
+        while(!stk2.isEmpty()){
+            stk1.push(stk2.pop());
+        }
+
+        return x;
+    }
+    
     public int peek() {
-        if (s2.isEmpty()) {
-            while (!s1.isEmpty()) {
-                s2.push(s1.pop());
+         if(!stk1.isEmpty()){
+            while(!stk1.isEmpty()){
+                stk2.push(stk1.pop());
             }
         }
-        return s2.peek();
-    }
+        int x = stk2.peek();
+        
+         while(!stk2.isEmpty()){
+            stk1.push(stk2.pop());
+        }
 
+        return x;
+    }
+    
     public boolean empty() {
-        return s1.isEmpty() && s2.isEmpty();
+        return stk1.isEmpty() && stk2.isEmpty();
     }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
