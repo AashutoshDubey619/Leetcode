06d57c7c88ea85e9e699a0e83bfc070e50c636d6
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-01-08 15:39:22 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (32.78%) - LeetSync

---
+++ leetcode/792-binary-search/binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return bs(nums, 0, nums.length - 1, target);
+    }
+
+    public int bs(int[] nums, int l, int r, int target) {
+        if (l > r) return -1;   
+
+        int m = l + (r - l) / 2;
+
+        if (nums[m] == target) {
+            return m;
+        } 
+        else if (nums[m] < target) {
+            return bs(nums, m + 1, r, target);
+        } 
+        else {
+            return bs(nums, l, m - 1, target);
+        }
+    }
+}
