21e18ae8dcf6d1fa57b4fc316bb96153521c3ae4
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-01-26 07:57:24 +0530

    Time: 22 ms (36.30%) | Memory: 64 MB (46.18%) - LeetSync

--- leetcode/1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ leetcode/1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -13,12 +13,7 @@ class Solution {
 
         for(int i=1;i<arr.length;i++){
             if(Math.abs(arr[i] - arr[i-1]) == mindif){
-                if(arr[i] > arr[i-1]){
-                res.add(Arrays.asList(arr[i-1] , arr[i]));
-                }
-                else{
-                res.add(Arrays.asList(arr[i] , arr[i-1]));
-                }
+                res.add(Arrays.asList(arr[i-1], arr[i]));
             }
         }
 
