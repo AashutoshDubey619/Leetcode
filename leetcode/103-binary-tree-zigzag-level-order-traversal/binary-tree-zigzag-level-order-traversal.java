6a5025e7673c8d231f9cbb054c1d865e47ed6997
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-01-26 11:06:31 +0530

    Time: 1 ms (81.62%) | Memory: 43.8 MB (52.73%) - LeetSync

--- leetcode/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ leetcode/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,55 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        
-        List<List<Integer>> res = new ArrayList<>();
 
+        List<List<Integer>> res = new ArrayList<>();
         if(root == null) return res;
 
         Queue<TreeNode> q = new LinkedList<>();
-
         q.offer(root);
+
+        boolean leftToRight = true;
 
         while(!q.isEmpty()){
             int size = q.size();
-            List<Integer> temp = new ArrayList<>();
+            List<Integer> temp = new ArrayList<>(size);
 
             for(int i=0;i<size;i++){
                 TreeNode node = q.poll();
-                temp.add(node.val);
+
+                if(leftToRight){
+                    temp.add(node.val);
+                } else {
+                    temp.add(0, node.val); 
+                }
 
                 if(node.left != null) q.offer(node.left);
                 if(node.right != null) q.offer(node.right);
             }
-            res.add(temp);
-        }
 
-        for(int i=1;i<res.size();i+=2){
-            if(res.get(i).size() > 1){
-                int l = 0;
-                int r = res.get(i).size() - 1;
-                while(l < r){
-                int temp = res.get(i).get(l);
-                res.get(i).set(l , res.get(i).get(r));
-                res.get(i).set(r , temp);
-                l++;
-                r--;
-                }
-            }
+            res.add(temp);
+            leftToRight = !leftToRight;
         }
 
         return res;
