22674a96de2492a816430e4b1b2296f8193d69dd
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-02-19 06:32:08 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (67.68%) - LeetSync

--- leetcode/2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ leetcode/2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -1,42 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        ListNode p1 = head;
-        ListNode p2 = p1.next;
+        ListNode curr = head;
 
-        while(p2 != null){
-            ListNode newnode = new ListNode(getGcd(p1.val , p2.val));
-            p1.next = newnode;
-            newnode.next = p2;
-            p1 = p2;
-            p2 = p2.next;
+        while(curr != null && curr.next != null){
+            int gcdValue = getGcd(curr.val, curr.next.val);
+            ListNode newNode = new ListNode(gcdValue);
+
+            newNode.next = curr.next;
+            curr.next = newNode;
+
+            curr = newNode.next;
         }
 
         return head;
     }
-    
-    public int getGcd(int a, int b){
-        if(a >= b){
-            for(int i=b;i>=1;i--){
-                if(a % i ==0 && b % i == 0) return i;
-            }
-        }
-        else if(a <= b){
-            for(int i=a;i>=1;i--){
-                if(a % i ==0 && b % i == 0) return i;
-            }
-        }
 
-        return -1;
+    public int getGcd(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
-    
 }
