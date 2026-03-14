5ce321f94df8862a69d0ce6e199a92f3666f5297
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-03-14 08:31:39 +0530

    Time: 10 ms (65.45%) | Memory: 46.6 MB (44.09%) - LeetSync

--- leetcode/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ leetcode/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i == j) dist[i][j] = 0;
-                else dist[i][j] = (int)1e9;
+                else dist[i][j] = Integer.MAX_VALUE;
             }
         }
 
@@ -22,7 +22,9 @@ class Solution {
         for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    dist[i][j] = Math.min(dist[i][j] , dist[i][k] + dist[k][j]);
+                    if(dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE){
+                        dist[i][j] = Math.min(dist[i][j] , dist[i][k] + dist[k][j]);
+                    }
                 }
             }
         }
