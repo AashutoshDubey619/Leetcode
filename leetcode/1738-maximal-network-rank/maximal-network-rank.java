ce0de35a5ed8bc38e4fb43f2de0339af4de7bd49
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-03-20 13:23:05 +0530

    Time: 4 ms (97.85%) | Memory: 47.3 MB (91.07%) - LeetSync

--- leetcode/1738-maximal-network-rank/maximal-network-rank.java
+++ leetcode/1738-maximal-network-rank/maximal-network-rank.java
@@ -1,41 +1,31 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-
-        List<HashSet<Integer>> adj = new ArrayList<>();
-
-        for(int i = 0; i < n; i++){
-            adj.add(new HashSet<>());
-        }
-
         int[] degree = new int[n];
+        boolean[][] connection = new boolean[n][n];
 
-        
-        for(int[] road : roads){
-            int u = road[0], v = road[1];
-
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-
+        for (int[] road : roads) {
+            int u = road[0];
+            int v = road[1];
             degree[u]++;
             degree[v]++;
+            connection[u][v] = true;
+            connection[v][u] = true; 
         }
 
-        int max = 0;
+        int rank = 0;
 
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-
-                int rank = degree[i] + degree[j];
-
-                if(adj.get(i).contains(j)){
-                    rank--; 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int currentRank = degree[i] + degree[j];
+                
+                if (connection[i][j]) {
+                    currentRank--;
                 }
 
-                max = Math.max(max, rank);
+                rank = Math.max(rank, currentRank);
             }
         }
 
-        return max;
+        return rank;
     }
 }
