6f109e4c9852fa0a2b78e8c0cec73614dfaa1703
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-03-25 06:52:23 +0530

    Time: 6 ms (25.17%) | Memory: 160 MB (93.71%) - LeetSync

--- leetcode/3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ leetcode/3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -6,36 +6,43 @@ class Solution {
 
         if(m == 1 && n == 1) return false;
 
-        long total_sum = 0;
+        long total = 0;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                total_sum += grid[i][j];
+                total += grid[i][j];
             }
         }
 
-        if(total_sum % 2 != 0) return false;
+        if(total % 2 != 0) return false;
 
-        long half = total_sum / 2;
+        long half = total / 2;
 
-        long rowprefsum = 0;
-        for(int i=0;i<m-1;i++){
-            long rowsum = 0;
+       
+        long[] rowSum = new long[m];
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                rowsum += grid[i][j];
+                rowSum[i] += grid[i][j];
             }
-            rowprefsum += rowsum;
-            if(rowprefsum == half) return true;
         }
 
-        long colprefsum = 0;
-        for(int j=0;j<n-1;j++){
-            long colsum = 0;
+        long prefix = 0;
+        for(int i=0;i<m-1;i++){
+            prefix += rowSum[i];
+            if(prefix == half) return true;
+        }
+
+        long[] colSum = new long[n];
+        for(int j=0;j<n;j++){
             for(int i=0;i<m;i++){
-                colsum += grid[i][j];
+                colSum[j] += grid[i][j];
             }
-            colprefsum += colsum;
-            if(colprefsum == half) return true;
+        }
+
+        prefix = 0;
+        for(int j=0;j<n-1;j++){
+            prefix += colSum[j];
+            if(prefix == half) return true;
         }
 
         return false;
