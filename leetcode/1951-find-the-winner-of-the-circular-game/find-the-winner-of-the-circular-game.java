90329847aac24bdd4b6310b847c216bbb181ea53
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-03-26 06:42:57 +0530

    Time: 3 ms (56.48%) | Memory: 42.7 MB (37.97%) - LeetSync

--- leetcode/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ leetcode/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -2,17 +2,12 @@ class Solution {
     public int findTheWinner(int n, int k) {
         
         ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++) list.add(i);
 
-        for(int i=0;i<n;i++)list.add(i+1);
-        
         int j = 0;
 
-        while(list.size() != 1){
-            int i = 0;
-            while(i < k-1){
-                j = (j+1)%list.size();
-                i++;
-            }
+        while(list.size() > 1){
+            j = (j + k - 1) % list.size();
             list.remove(j);
         }
 
