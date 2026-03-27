3edd32d1715888d932d59570f609a4dd226fc5e7
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-03-27 07:28:16 +0530

    Time: 1 ms (99.47%) | Memory: 46.9 MB (64.00%) - LeetSync

--- leetcode/3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ leetcode/3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -1,48 +1,20 @@
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
-        int m = mat.length;
-        int n = mat[0].length;
 
-        int[][] org = new int[m][n];
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                org[i][j] = mat[i][j];
+        int m=mat.length;
+        int n=mat[0].length;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int ind=(j+k)%n;
+                if(mat[i][j]!=mat[i][ind])
+                {
+                    return false;
+                }
             }
         }
-
-        k = k % n;
-
-        for(int i=0;i<m;i++){
-            if(i % 2 == 0){
-                reverse(i ,0 , k-1 , mat);
-                reverse(i ,k , n-1 , mat);
-                reverse(i ,0 , n-1 , mat);
-            }
-            else{
-                reverse(i ,0 , n-1 , mat);
-                reverse(i ,0 , k-1 , mat);
-                reverse(i ,k , n-1 , mat);
-            }
-        }
-      
-      for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(mat[i][j] != org[i][j])return false;
-        }
-      }
-
-      return true;
-    }
-
-    public void reverse(int i , int l , int r , int[][] mat){
-        while(l < r){
-            int temp = mat[i][l];
-            mat[i][l] = mat[i][r];
-            mat[i][r] = temp;
-
-            l++;
-            r--;
-        }
+        return true;
     }
 }
