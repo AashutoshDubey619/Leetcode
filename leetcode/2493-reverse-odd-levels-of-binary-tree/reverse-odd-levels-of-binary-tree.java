9b060354b4a91a3665e5d7296082ce31993a5b94
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-03-30 11:53:34 +0530

    Time: 11 ms (22.89%) | Memory: 51.1 MB (33.92%) - LeetSync

--- leetcode/2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ leetcode/2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -1,76 +1,38 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
         
-        if(root.left == null && root.right == null)return root;
-
         Queue<TreeNode> q = new LinkedList<>();
-
-        ArrayList<ArrayList<Integer>> vals = new ArrayList<>();
-
-        
         q.add(root);
 
         int level = 0;
 
         while(!q.isEmpty()){
             int size = q.size();
+            List<TreeNode> list = new ArrayList<>();
 
-            vals.add(new ArrayList<>());
-
-            for(int s=0;s<size;s++){
+            for(int i = 0; i < size; i++){
                 TreeNode node = q.poll();
-                vals.get(level).add(node.val);
+                list.add(node);
 
-                if(node.left != null)q.add(node.left);
-                if(node.right != null)q.add(node.right);
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+
+            if(level % 2 == 1){
+                int i = 0, j = list.size() - 1;
+
+                while(i < j){
+                    int temp = list.get(i).val;
+                    list.get(i).val = list.get(j).val;
+                    list.get(j).val = temp;
+                    i++;
+                    j--;
+                }
             }
 
             level++;
         }
-
-        for(int i=0;i<vals.size();i++){
-            if(i % 2 == 1){
-                Collections.reverse(vals.get(i));
-            }
-        }
-
-        level = 0;
-
-        q.add(root);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-            
-            for(int s=0;s<size;s++){
-                TreeNode node = q.poll();
-
-                node.val = vals.get(level).get(s);
-
-                if(node.left != null)q.add(node.left);
-                if(node.right != null)q.add(node.right);
-            }
-
-            level++;
-        }
-
-
 
         return root;
-
     }
 }
