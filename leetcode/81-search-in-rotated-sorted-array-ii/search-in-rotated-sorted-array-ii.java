13e73387bf36d616100b88c81d3379cf2cda3b3d
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-04-04 17:46:24 +0530

    Time: 2 ms (3.76%) | Memory: 46.7 MB (7.44%) - LeetSync

---
+++ leetcode/81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int n : nums)set.add(n);
+
+        return set.contains(target);
+    }
+}
