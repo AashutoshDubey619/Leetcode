f73f74c5915bf69d566fab30198f423b3d0a57b1
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-04-10 22:35:35 +0530

    Time: 9 ms (12.41%) | Memory: 47.5 MB (88.78%) - LeetSync

---
+++ leetcode/1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumEvenGrandparent(TreeNode root) {
+        
+        int sum = 0;
+
+        if(root.left == null && root.right == null)return sum;
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+
+        while(q.size() > 0){
+            TreeNode node = q.poll();
+
+            if(node.left != null)q.add(node.left);
+            if(node.right != null)q.add(node.right);
+
+            if(node.val % 2 == 0){
+                boolean flag = false;
+                if(node.left != null){
+                    if(node.left.left != null){
+                        sum += node.left.left.val;
+                        flag = true;
+                        }
+                    if(node.left.right != null){
+                        sum += node.left.right.val;
+                        flag = true;
+                        }
+                }
+                if(node.right != null){
+                    if(node.right.left != null){
+                        sum += node.right.left.val;
+                        flag = true;
+                        }
+                    if(node.right.right != null){
+                        sum += node.right.right.val;
+                        flag = true;
+                        }  
+                }
+
+            }
+        }
+
+        return sum;
+    }
+}
