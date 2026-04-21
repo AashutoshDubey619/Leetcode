c2d9dda6cf420b1ed8289fdeb09ced1b7e613499
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-04-21 15:01:10 +0530

    Time: 2 ms (67.69%) | Memory: 85.3 MB (24.52%) - LeetSync

--- leetcode/932-monotonic-array/monotonic-array.java
+++ leetcode/932-monotonic-array/monotonic-array.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
 
-     boolean inc = true;
-     boolean dec = true;
+        boolean inc = true;
+        boolean dec = true;
 
-     for(int i=0;i<nums.length-1;i++){
-        if(nums[i] > nums[i+1]){
-            inc = false;
-            break;
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] > nums[i + 1]) {
+                inc = false;
+            }
+            if(nums[i] < nums[i + 1]) {
+                dec = false;
+            }
         }
-     }
 
-     for(int i=0;i<nums.length-1;i++){
-        if(nums[i] < nums[i+1]){
-            dec = false;
-            break;
-        }
-     }
-     
-
-     return inc || dec;
+        return inc || dec;
     }
 }
