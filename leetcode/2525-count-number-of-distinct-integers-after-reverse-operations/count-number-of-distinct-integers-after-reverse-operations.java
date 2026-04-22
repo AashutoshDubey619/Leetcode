231281d59618e70ffc1455c5b6a7e8b01623d3be
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-04-23 05:21:36 +0530

    Time: 53 ms (91.07%) | Memory: 105.5 MB (77.26%) - LeetSync

--- leetcode/2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
+++ leetcode/2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
@@ -3,10 +3,9 @@ class Solution {
         
         HashSet<Integer> set = new HashSet<>();
 
-        for(int n : nums)set.add(n);
-
         for(int i=0;i<nums.length;i++){
             int rev = 0;
+            set.add(nums[i]);
 
             while(nums[i] > 0){
                 int ld = nums[i] % 10;
