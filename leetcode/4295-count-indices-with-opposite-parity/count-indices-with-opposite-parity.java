3e59711782f996784a374f15bbc6f173fb9068fb
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-05-03 12:31:48 +0530

    Time: 5 ms (25.18%) | Memory: 46.7 MB (94.74%) - LeetSync

---
+++ leetcode/4295-count-indices-with-opposite-parity/count-indices-with-opposite-parity.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] countOppositeParity(int[] nums) {
+        
+        int[] ans = new int[nums.length];
+
+        for(int i=0;i<nums.length-1;i++){
+            int count = 0;
+
+            for(int j=i+1;j<nums.length;j++){
+                if((nums[i] % 2 == 0 && nums[j] % 2 != 0) || (nums[i] % 2 != 0 && nums[j] % 2 == 0))count++;
+            }
+
+            ans[i] = count;
+        }
+
+        ans[ans.length-1] = 0;
+
+        return ans;
+    }
+}
