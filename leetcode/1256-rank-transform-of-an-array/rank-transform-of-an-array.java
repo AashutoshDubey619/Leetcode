afb4f0ca698324ba41bd2e42dd6671d4ef4d8626
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-05-05 13:00:24 +0530

    Time: 29 ms (86.51%) | Memory: 77.2 MB (21.17%) - LeetSync

--- leetcode/1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ leetcode/1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        
-        int ans[] = new int[arr.length];
-
-        if(arr.length == 0) return ans;
-        HashMap<Integer , Integer> map = new HashMap<>();
-        HashSet<Integer> set = new HashSet<>();
-
-
-       for(int x  : arr)set.add(x);
-
-        ArrayList<Integer> list = new ArrayList<>(set);
-
-        Collections.sort(list);
-
-        for(int i=0;i<list.size();i++){
-            map.put(list.get(i) , i + 1);
+        int n=arr.length;
+        int[] nums=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=arr[i];
         }
-
-
-        for(int i=0;i<arr.length;i++){
-            ans[i] = map.get(arr[i]);
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int rank=1;
+        for(int i=0;i<n;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],rank);
+                rank++;
+            }
         }
-
-        return ans;
+        for(int i=0;i<n;i++){
+            arr[i]=map.get(arr[i]);
+        }
+        return arr;
     }
 }
