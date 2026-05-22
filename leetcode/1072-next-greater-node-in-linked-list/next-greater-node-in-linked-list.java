893b31306fc41ce7199446c42515449201c264ee
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-05-22 10:37:31 +0530

    Time: 14 ms (95.26%) | Memory: 50.5 MB (11.07%) - LeetSync

---
+++ leetcode/1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+
+        ListNode temp = head;
+
+        int len = 0;
+
+        while(temp != null){
+            len++;
+            temp = temp.next;
+        }
+
+        temp = head;
+
+        int[] nodes = new int[len];
+        int[] res = new int[len];
+
+        int j = 0;
+         while(temp != null){
+            nodes[j] = temp.val;
+            j++;
+            temp = temp.next;
+        }
+
+        Stack<Integer> stk = new Stack<>();
+
+        for(int i=0;i<nodes.length;i++){
+            while(!stk.isEmpty() && nodes[stk.peek()] < nodes[i]){
+                res[stk.pop()] = nodes[i];
+            }
+            stk.push(i);
+        }
+
+        return res;
+    }
+}
