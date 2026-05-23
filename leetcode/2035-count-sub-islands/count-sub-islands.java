828205bba4d23cfd919020e119c237d5bb19ba9a
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-05-23 12:44:50 +0530

    Time: 54 ms (5.87%) | Memory: 117.4 MB (85.69%) - LeetSync

--- leetcode/2035-count-sub-islands/count-sub-islands.java
+++ leetcode/2035-count-sub-islands/count-sub-islands.java
@@ -1,86 +1,89 @@
 class Solution {
-    class Pair{
-        int row , col;
-
-        Pair(int row , int col){
-            this.row = row;
-            this.col = col;
-        }
-    }
 
     int count = 0;
 
     boolean[][] vis1;
+
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        
+
         int m = grid1.length;
         int n = grid1[0].length;
 
         vis1 = new boolean[m][n];
         boolean[][] vis2 = new boolean[m][n];
 
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!vis1[i][j] && grid1[i][j] == 1){
-                    bfs(i , j , vis1 , grid1 , 1);
+                if(!vis1[i][j] && grid1[i][j] == 1) {
+                    bfs(i, j, vis1, grid1, 1);
                 }
             }
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!vis2[i][j] && grid2[i][j] == 1){
-                    bfs(i , j , vis2 , grid2 , 2);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(!vis2[i][j] && grid2[i][j] == 1) {
+                    bfs(i, j, vis2, grid2, 2);
                 }
             }
         }
 
-        
         return count;
-
     }
 
-    public void bfs(int i , int j , boolean[][] vis , int[][] grid , int version){
+    public void bfs(int i, int j, boolean[][] vis, int[][] grid, int version) {
+
         int m = grid.length;
         int n = grid[0].length;
 
-        Queue<Pair> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
+
         boolean flag = true;
-        
-        q.add(new Pair(i , j));
+
+        q.offer(i * n + j);
 
         vis[i][j] = true;
 
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int row = p.row , col = p.col;
+        while(!q.isEmpty()) {
 
-            if(version == 2 && !vis1[row][col]){
+            int cell = q.poll();
+
+            int row = cell / n;
+            int col = cell % n;
+
+            if(version == 2 && !vis1[row][col]) {
                 flag = false;
             }
 
+            if(row > 0 && !vis[row - 1][col] && grid[row - 1][col] == 1) {
 
-            if(row > 0 && !vis[row-1][col] && grid[row-1][col] == 1){
-                q.add(new Pair(row-1 , col));
-                vis[row-1][col] = true;
-            }
-            if(row < m-1 && !vis[row+1][col] && grid[row+1][col] == 1){
-                 q.add(new Pair(row+1 , col));
-                 vis[row+1][col] = true;
+                vis[row - 1][col] = true;
+                q.offer((row - 1) * n + col);
             }
 
-            if(col > 0 && !vis[row][col-1] && grid[row][col-1] == 1){
-                q.add(new Pair(row , col-1));
-                vis[row][col-1] = true;
+            if(row < m - 1 && !vis[row + 1][col] && grid[row + 1][col] == 1) {
+
+                vis[row + 1][col] = true;
+                q.offer((row + 1) * n + col);
             }
-            if(col < n-1 && !vis[row][col+1] && grid[row][col+1] == 1){
-                 q.add(new Pair(row , col+1));
-                 vis[row][col+1] = true;
+
+            if(col > 0 && !vis[row][col - 1] && grid[row][col - 1] == 1) {
+
+                vis[row][col - 1] = true;
+                q.offer(row * n + (col - 1));
+            }
+
+            if(col < n - 1 && !vis[row][col + 1] && grid[row][col + 1] == 1) {
+
+                vis[row][col + 1] = true;
+                q.offer(row * n + (col + 1));
             }
         }
 
-        if(version == 2 && flag) count++;
+        if(version == 2 && flag) {
+            count++;
+        }
     }
-    
 }
