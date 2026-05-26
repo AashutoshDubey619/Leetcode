67598bbf1dbe1b3d0b464ea8377f66becc4e5d82
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-05-26 10:35:16 +0530

    Time: 15 ms (41.25%) | Memory: 47.4 MB (36.50%) - LeetSync

--- leetcode/4313-password-strength/password-strength.java
+++ leetcode/4313-password-strength/password-strength.java
@@ -7,9 +7,9 @@ class Solution {
 
         for(char c : password.toCharArray()){
             if(set.add(c)){
-                if(c >= 'a' && c <= 'z')strength += 1;
-                else if(c >= 'A' && c<='Z')strength += 2;
-                else if(c >= '0' && c <= '9')strength += 3;
+                if(Character.isLowerCase(c)) strength += 1;
+                else if(Character.isUpperCase(c)) strength += 2;
+                else if(Character.isDigit(c)) strength += 3;
                 else strength += 5;
             }
         }
