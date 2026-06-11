b8bcc9ea3b393c5bd3ba269e8427d827196d9585
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-06-11 08:31:36 +0530

    Time: 121 ms (6.66%) | Memory: 75.5 MB (18.41%) - LeetSync

--- leetcode/416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ leetcode/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,43 +1,42 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        
         int sum = 0;
 
-        for(int el : nums)sum += el;
+        for(int x : nums)sum += x;
 
         if(sum % 2 != 0)return false;
 
-        int[][] dp = new int[nums.length][sum/2 + 1];
+        int[][] dp = new int[nums.length][(sum/2)+1];
 
-        for(int i=0;i<dp.length;i++)Arrays.fill(dp[i] , -1);
+        for(int[] d : dp)Arrays.fill(d , -1);
 
-        return subset(0 , sum/2 , dp , nums);
+        return subset(0 , nums , sum/2 , dp);
     }
 
-    public boolean subset(int i , int target , int[][] dp , int[] nums){
+    public boolean subset(int i , int[] nums , int target , int[][] dp){
         if(i == nums.length){
-            return target == 0;
+            if(target == 0)return true;
+            else return false;
         }
 
         if(dp[i][target] != -1){
-            return dp[i][target] == 1;
+            if(dp[i][target] == 0)return true;
+            else return false;
         }
 
-        boolean skip = subset(i + 1 , target , dp , nums);
-
-        boolean ans = false;
-
-        if(target - nums[i] < 0){
-            if(skip)dp[i][target] = 1;
-            else dp[i][target] = 0;
-            return skip;
-        }
-        else{
-            boolean pick = subset(i+1 , target - nums[i] , dp , nums);
-            ans = pick || skip;
-        }
+        boolean skip = subset(i + 1 , nums , target , dp);
         
-        if(ans)dp[i][target] = 1;
-        else dp[i][target] = 0;
+        if(target - nums[i] < 0)return skip;
+      
+        boolean pick = subset(i + 1 , nums , target - nums[i] , dp);
+        
+        boolean ans = pick || skip;
+
+        if(ans){
+            dp[i][target] = 0;
+        }
+        else dp[i][target] = 1;
 
         return ans;
     }
