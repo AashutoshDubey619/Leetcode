9775e730126287ba0aa44d3554b27844b6cdbe3d
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-06-11 08:45:04 +0530

    Time: 44 ms (5.95%) | Memory: 48.1 MB (5.14%) - LeetSync

--- leetcode/322-coin-change/coin-change.java
+++ leetcode/322-coin-change/coin-change.java
@@ -1,28 +1,27 @@
 class Solution {
-
     public int coinChange(int[] coins, int amount) {
-
+        
         long[][] dp = new long[coins.length][amount+1];
 
-        for(int i=0;i<dp.length;i++)Arrays.fill(dp[i] , -1);
+        for(long d[] : dp)Arrays.fill(d , -1);
 
-        if(countCoin(0 , coins , amount , dp) == Integer.MAX_VALUE)return -1;
-        else return (int)countCoin(0 , coins , amount , dp);
+        if(countCoin(0 , coins , amount ,dp)== Integer.MAX_VALUE)return -1;
+        return (int)countCoin(0 , coins , amount ,dp);
     }
 
-   public long countCoin(int i , int[] coins , int amount , long[][] dp){
+    public long countCoin(int i , int[] coins , int amount , long[][] dp){
         if(i == coins.length){
             if(amount == 0)return 0;
-            else return Integer.MAX_VALUE;
-        }
+            return Integer.MAX_VALUE;
+        } 
 
         if(dp[i][amount] != -1)return dp[i][amount];
 
-        long skip = countCoin(i+1 , coins , amount, dp);
- 
-        if(amount < coins[i])return skip;
+        long skip = countCoin(i + 1 , coins , amount , dp);
 
-        long pick = 1 + countCoin(i , coins , amount-coins[i] , dp);
+        if(amount - coins[i] < 0)return skip;
+
+        long pick = 1 + countCoin(i , coins , amount - coins[i] , dp);
 
         return dp[i][amount] = Math.min(pick , skip);
     }
