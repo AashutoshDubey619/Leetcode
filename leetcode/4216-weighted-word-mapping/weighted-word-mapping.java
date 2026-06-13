cf50ecf8faaa52f246c9831b66bb718349beb667
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-06-13 06:29:09 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (81.55%) - LeetSync

---
+++ leetcode/4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        
+        StringBuilder sb = new StringBuilder();
+
+        for(String word : words){
+            int weight = 0;
+
+            for(int i=0;i<word.length();i++){
+                weight += weights[word.charAt(i) - 'a'];
+            }
+
+            weight = weight % 26;
+
+            sb.append((char)('z' - weight));
+        }
+
+        return sb.toString();
+    }
+}
