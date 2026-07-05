d529bd8e5f5e1b242c58168dfadadc001b43c59c
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-07-05 11:18:24 +0530

    Time: 3 ms (79.98%) | Memory: 44.1 MB (79.45%) - LeetSync

--- leetcode/257-binary-tree-paths/binary-tree-paths.java
+++ leetcode/257-binary-tree-paths/binary-tree-paths.java
@@ -15,31 +15,27 @@
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        ArrayList<String> res = new ArrayList<>();
+        List<String> res = new ArrayList<>();
 
-        if(root == null) return res;
-        if(root.left == null && root.right == null){
-            res.add(String.valueOf(root.val));
-            return res;
-        }
-        
-        String s = root.val + "->";
-        
-        paths(root.left , res , s);
-        paths(root.right , res , s);
+        paths(root , new StringBuilder() , res);
 
         return res;
     }
 
-
-    public void paths(TreeNode root , ArrayList<String> res , String s){
-
-        if(root == null) return;
-        if(root.left == null && root.right == null){
-            res.add(s + String.valueOf(root.val));
+    public void paths(TreeNode root , StringBuilder sb , List<String> res ){
+        if(root == null)return;
+        else if(root.left == null && root.right == null){
+            int len = sb.length();
+            sb.append(root.val);
+            res.add(sb.toString());
+            sb.setLength(len);
+            return;
         }
-        
-        paths(root.left , res , s + root.val + "->");
-        paths(root.right , res , s + root.val + "->");
+
+        int len = sb.length();
+        sb.append(root.val+"->");
+        paths(root.left , sb , res);
+        paths(root.right , sb , res);
+        sb.setLength(len);
     }
 }
