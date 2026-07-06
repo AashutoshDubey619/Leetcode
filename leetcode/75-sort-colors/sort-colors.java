e478c2db0412f61b7c69c33c38534ec0958d76e7
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-07-06 11:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (10.66%) - LeetSync

--- leetcode/75-sort-colors/sort-colors.java
+++ leetcode/75-sort-colors/sort-colors.java
@@ -1,16 +1,14 @@
 class Solution {
     public void sortColors(int[] nums) {
+        int [] freq = new int[3];
 
-        int[] freq = new int[3];
+        for(int x : nums)freq[x]++;
+        int k = 0;
 
-        for(int x : nums){
-            freq[x]++;
-        }
-
-        int idx = 0;
-        for(int color = 0; color < 3; color++){
-            while(freq[color]-- > 0){
-                nums[idx++] = color;
+        for(int i=0;i<freq.length;i++){
+            while(freq[i] > 0){
+                nums[k++] = i;
+                freq[i]--;
             }
         }
     }
