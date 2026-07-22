61e09342f220354e3477a124a662c300d445d63c
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-07-22 08:36:07 +0530

    Time: 6 ms (70.46%) | Memory: 46.6 MB (28.99%) - LeetSync

--- leetcode/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ leetcode/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,24 +1,25 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
-        int max_len = 0;
-        int i = 0;
-        HashSet<Character> set = new HashSet<>();
-        int j = 0;
         
+        HashSet<Character> set = new HashSet<>();
+        int longest = 1;
+
+        if(s.length() == 0)return 0;
+
+        int i = 0 , j = 0;
+
         while(j < s.length()){
-          if(!set.contains(s.charAt(j))){
-                set.add(s.charAt(j));
-            max_len = Math.max(max_len , j - i + 1);
-            j++;
-            }
-            else{   
+            
+            while(!set.add(s.charAt(j))){
                 set.remove(s.charAt(i));
                 i++;
             }
+
+            longest = Math.max(longest , j - i + 1);
+            
+            j++;
         }
 
-        return max_len;
-
+        return longest;
     }
 }
