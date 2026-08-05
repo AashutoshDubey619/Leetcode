0d004539b4095a944b95a23ac5b59345eb2ad0b4
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-08-05 06:17:45 +0530

    Time: 55 ms (92.31%) | Memory: 281.3 MB (73.08%) - LeetSync

---
+++ leetcode/3561-remove-methods-from-project/remove-methods-from-project.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        boolean[] vis = new boolean[n];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+
+        for (int[] inv : invocations)
+            adj.get(inv[0]).add(inv[1]);
+
+        bfs(k, vis, adj);
+
+        for (int i = 0; i < n; i++) {
+            if (vis[i]) continue;
+
+            for (int el : adj.get(i)) {
+                if (vis[el]) {
+                    List<Integer> ans = new ArrayList<>();
+                    for (int j = 0; j < n; j++)
+                        ans.add(j);
+                    return ans;
+                }
+            }
+        }
+
+        // Warna sirf non-suspicious methods return karo
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!vis[i])
+                res.add(i);
+        }
+
+        return res;
+    }
+
+    public void bfs(int k, boolean[] vis, ArrayList<ArrayList<Integer>> adj) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(k);
+        vis[k] = true;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int nxt : adj.get(node)) {
+                if (!vis[nxt]) {
+                    vis[nxt] = true;
+                    q.offer(nxt);
+                }
+            }
+        }
+    }
+}
