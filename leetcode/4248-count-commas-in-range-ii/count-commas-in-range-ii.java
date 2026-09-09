416dfc4bb5c4c3b5ba6501dbd09f7a867ae085ad
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-09-09 07:22:43 +0530

    Time: 1 ms (99.36%) | Memory: 42.7 MB (56.41%) - LeetSync

---
+++ leetcode/4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long countCommas(long n) {
+
+        long ans = 0;
+        long base = 1000;
+
+        while(base <= n){
+            ans += n - base + 1;
+            base *= 1000;
+        }
+
+        return ans;
+    }
+}
