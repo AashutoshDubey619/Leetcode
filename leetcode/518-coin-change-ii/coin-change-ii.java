ff67e03ece8e4be892319810a3e4f338f6700828
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-09-16 15:48:24 +0530

    Time: 28 ms (19.64%) | Memory: 56.4 MB (54.72%) - LeetSync

--- leetcode/518-coin-change-ii/coin-change-ii.java
+++ leetcode/518-coin-change-ii/coin-change-ii.java
@@ -1,27 +1,34 @@
 class Solution {
     public int change(int amount, int[] coins) {
         
-       int[][] dp = new int[coins.length][amount+1];
+        int n = coins.length;
 
-       for(int[] d : dp)Arrays.fill(d , -1);
+        int dp[][] = new int[coins.length + 1][amount+1];
 
-       return ways(0 , amount , coins , dp);
+        for(int d[] : dp)Arrays.fill(d , -1);
+ 
+        if(ways(0 , amount , coins , n , dp) == Integer.MAX_VALUE)return 0;
+        return ways(0 , amount , coins , n , dp);
     }
 
-    public int ways(int i , int amount , int[] coins ,int[][] dp){
-        if(i == coins.length){
+    public int ways(int i , int amount , int[] coins , int n ,int[][] dp){
+        if(i == n){
             if(amount == 0)return 1;
-            else return 0;
+            else return Integer.MAX_VALUE;
         }
+
+        if(amount == 0)return 1;
 
         if(dp[i][amount] != -1)return dp[i][amount];
 
-        int skip = ways(i+1 , amount , coins , dp);
+        int skip = ways(i + 1 , amount , coins , n , dp);
 
         if(amount < coins[i])return dp[i][amount] = skip;
 
-        int pick = ways(i , amount - coins[i], coins , dp);
+        int pick = ways(i , amount - coins[i] , coins , n , dp);
 
-        return dp[i][amount] = skip + pick;
+        if(skip == Integer.MAX_VALUE)return dp[i][amount] = pick;
+        else if(pick == Integer.MAX_VALUE)return dp[i][amount] = skip;
+        else return dp[i][amount] = skip + pick;
     }
 }
