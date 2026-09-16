e5aa3e7848fa6f835ea4101c0b4a52c45980fd9e
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-09-16 15:35:58 +0530

    Time: 22 ms (45.83%) | Memory: 54 MB (36.29%) - LeetSync

--- leetcode/1250-longest-common-subsequence/longest-common-subsequence.java
+++ leetcode/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,20 +1,23 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         
-        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        int m = text1.length();
+        int n = text2.length();
 
-        for(int d[] : dp)Arrays.fill(d , -1);
+        int[][] dp = new int[m+1][n+1];
 
-        return lcs(text1, text1.length(),text2, text2.length() , dp);
+        for(int [] d : dp)Arrays.fill(d , -1);
+
+        return lcs(text1 , m , text2 , n , dp);
     }
 
-    public int lcs(String text1, int m, String text2, int n , int[][] dp) {
-        if(m == 0 || n == 0)return 0;
+    public int lcs(String s1, int m , String s2 , int n ,int[][] dp){
+        if(m <= 0 || n <= 0)return 0;
 
         if(dp[m][n] != -1)return dp[m][n];
 
-        if(text1.charAt(m-1) == text2.charAt(n-1)) return dp[m][n] = 1 + lcs(text1, m-1, text2, n-1 , dp);
+        if(s1.charAt(m-1) == s2.charAt(n-1)) return dp[m][n] =  1 + lcs(s1 , m-1 , s2 , n-1 , dp);
 
-        return dp[m][n] = Math.max(lcs(text1, m-1, text2, n , dp),lcs(text1, m, text2, n-1 , dp));
+        return dp[m][n] = Math.max(lcs(s1 , m-1 , s2 , n , dp) , lcs(s1 , m , s2 , n-1 , dp));
     }
 }
