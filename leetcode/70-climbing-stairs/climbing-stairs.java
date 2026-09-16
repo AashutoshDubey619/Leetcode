73e6a3a84c2fe31e4265cf9a21ef57064794e047
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-09-16 10:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.15%) - LeetSync

--- leetcode/70-climbing-stairs/climbing-stairs.java
+++ leetcode/70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,19 @@
 class Solution {
     public int climbStairs(int n) {
+        if(n <= 2)return n;
+
         int [] dp = new int[n+1];
 
         Arrays.fill(dp , -1);
 
-        return climb(n, dp);
+        return paths(n , dp);
     }
 
-    public int climb(int n , int[] dp){
-        if(n == 1 || n == 2)return n;
+    public int paths(int n ,int[] dp){
+        if(n <= 2)return n;
 
         if(dp[n] != -1)return dp[n];
 
-        return dp[n] = climb(n-1 , dp) + climb(n-2 , dp);
+        return dp[n] = paths(n-1 , dp) + paths(n-2 , dp);
     }
 }
