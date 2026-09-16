fd8c9c6c5f87ad8ad6e706dc363920f48d42dd90
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-09-16 15:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (65.43%) - LeetSync

--- leetcode/62-unique-paths/unique-paths.java
+++ leetcode/62-unique-paths/unique-paths.java
@@ -1,19 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
+        
+        int dp[][] = new int[m+1][n+1];
 
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i] , -1);
-        }
-        return paths(0 , 0 ,m , n , dp);
+        for(int d[] : dp)Arrays.fill(d , -1);
+
+        return paths(0 , 0 , m , n , dp);
     }
 
-    public int paths(int row , int col ,int m,int n , int[][] dp){
-        if(row >= m || col >= n )return 0;
-        if(row == m-1 || col == n-1) return 1;
+    public int paths(int row , int col , int m , int n ,int[][] dp){
+        if(row == m-1 || col == n-1)return 1;
 
         if(dp[row][col] != -1)return dp[row][col];
 
-        return dp[row][col] = paths(row+1 , col ,m, n ,dp) + paths(row , col+1,m ,n , dp);
+        return dp[row][col] = paths(row , col+1 , m , n , dp) + paths(row + 1 , col , m , n , dp);
     }
 }
