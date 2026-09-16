c7ac0f8d63ac6789ee89c80fa902d7e8ad073b92
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-09-16 12:09:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (22.68%) - LeetSync

--- leetcode/213-house-robber-ii/house-robber-ii.java
+++ leetcode/213-house-robber-ii/house-robber-ii.java
@@ -1,29 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
         
+        if(nums.length == 1)return nums[0];
+
         int n = nums.length;
-        int dp[] = new int[n];
 
-        if(n == 1)return nums[0];
-        
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0] , nums[1]);
+        int[] dp1 = new int[n+1];
+        int[] dp2 = new int[n+1];
 
-        for(int i=2;i<n-1;i++){
-            dp[i] = Math.max(nums[i] + dp[i-2] , dp[i-1]);
-        }
+        Arrays.fill(dp1 , -1);
+        Arrays.fill(dp2 , -1);
 
-        int max = dp[n-2];
+        return Math.max(maxRob(0, n-1, nums , dp1) , maxRob(1 , n , nums , dp2));
+    }
 
-        dp[n-1] = nums[n-1];
-        dp[n-2] = Math.max(nums[n-1] , nums[n-2]);
-        
-        for(int i=n-3;i>=1;i--){
-            dp[i] = Math.max(nums[i] + dp[i+2] , dp[i+1]);
-        }
+    public int maxRob(int i , int n , int[] nums ,int[] dp){
+        if(i >= n)return 0;
 
-        max = Math.max(max , dp[1]);
+        if(dp[i] != -1)return dp[i];
 
-        return max;
+        int pick = nums[i] + maxRob(i + 2, n , nums , dp);
+
+        int skip = maxRob(i + 1 , n , nums , dp);
+
+        return dp[i] = Math.max(skip , pick);
     }
 }
