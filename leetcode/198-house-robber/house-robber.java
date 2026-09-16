a467a109ab9a642a05fc60e98e9611b1871c949a
Aashutosh Dubey <aashutoshd85@gmail.com>
2026-09-16 11:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.88%) - LeetSync

--- leetcode/198-house-robber/house-robber.java
+++ leetcode/198-house-robber/house-robber.java
@@ -1,20 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
+        
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return rob_max(n-1, nums, dp);
+
+        int[] dp = new int[n + 1];
+
+        Arrays.fill(dp , -1);
+
+        return maxRob(0 , 0 , n , nums , dp);
     }
 
-    public int rob_max(int idx, int[] nums, int[] dp){
-        if(idx == 0) return nums[0];
-        if(idx < 0) return 0;
+    public int maxRob(int i , int amount , int n , int[] nums ,int[] dp){
+        if(i >= n)return amount;
 
-        if(dp[idx] != -1) return dp[idx];
+        if(dp[i] != -1)return dp[i];
 
-        int take = nums[idx] + rob_max(idx-2, nums, dp);
-        int skip = rob_max(idx-1, nums, dp);
+        int pick = nums[i] + maxRob(i+2 , amount , n , nums , dp);
 
-        return dp[idx] = Math.max(take, skip);
+        int skip = maxRob(i+1 , amount , n , nums , dp);
+
+        return dp[i] = Math.max(pick , skip);
     }
 }
